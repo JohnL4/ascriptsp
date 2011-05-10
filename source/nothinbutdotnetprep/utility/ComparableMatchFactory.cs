99afa62cabd85b74010c4aadcfa4d72bb8d6fe6c
John Lusk <john-training@how-hard-can-it-be.com>
2011-05-10 14:34:20 -0400

    Pushing new changes

---
+++ source/nothinbutdotnetprep/utility/ComparableMatchFactory.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace nothinbutdotnetprep.utility
+{
+    public class ComparableMatchFactory<ItemToCreateCriteriaFor, PropertyType> where PropertyType : IComparable<PropertyType>
+    {
+        PropertyAccessor<ItemToCreateCriteriaFor, PropertyType> accessor;
+    
+        public ComparableMatchFactory(PropertyAccessor<ItemToCreateCriteriaFor, PropertyType> accessor)
+        {
+            this.accessor = accessor;
+        }
+
+        public IMatch<ItemToCreateCriteriaFor> greater_than( PropertyType value)
+        {
+            return new AnonymousCriteria<ItemToCreateCriteriaFor>(
+                x => accessor(x).CompareTo(value) > 0
+                );
+        }
+    }
+}
--- source/nothinbutdotnetprep/utility/Where.cs
+++ source/nothinbutdotnetprep/utility/Where.cs
@@ -1,4 +1,6 @@
-﻿namespace nothinbutdotnetprep.utility
+﻿using System;
+
+namespace nothinbutdotnetprep.utility
 {
     public class Where<ItemToMatch>
     {
@@ -6,6 +8,12 @@
             PropertyAccessor<ItemToMatch, PropertyType> accessor)
         {
             return new MatchFactory<ItemToMatch, PropertyType>(accessor);
+        }
+
+        public static ComparableMatchFactory<ItemToMatch, PropertyType> has_an<PropertyType>(
+            PropertyAccessor<ItemToMatch,PropertyType> accessor) where PropertyType : IComparable<PropertyType>
+        {
+            return new ComparableMatchFactory<ItemToMatch, PropertyType>(accessor);
         }
     }
 }
